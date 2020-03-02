3f0726ee971eb71e1e239fa5f33e32a65aa49e32
shivanimoray <morayshivani@gmail.com>
2020-03-02 16:43:45 +0100

    Navbar and Hero component

--- online_gift_store/src/App.js
+++ online_gift_store/src/App.js
@@ -1,16 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'; //react router dom
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1> Wrapped Wonders</h1>
-      </header>
-    </div>
-  );
-}
+//pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Categories from './pages/Categories';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Error from './pages/Error';
 
-export default App;
+//components
+import Header from './components/Header';
+
+export default function App(){
+  return <Router>
+  <Header />
+  <Switch>
+  <Route exact path="/"> <Home /> </Route>
+    <Route path="/about"> <About /> </Route>
+    <Route path="/categories"> <Categories /> </Route>
+    <Route path="/cart"> <Cart /> </Route>
+    <Route path="/login"> <Login /> </Route>
+    <Route path="*"> <Error /> </Route>
+  </Switch>
+  </Router>
+}  
+
---
+++ online_gift_store/src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Hero() {
+  return <div className="hero">
+    <div className="banner">
+      <h1>Amazing gifts with a lot of <br /> options to choose from..!!</h1>
+      <p>Choose gifts for every occassion</p>
+    </div>
+  </div>
+}
