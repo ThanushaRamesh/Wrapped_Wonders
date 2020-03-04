22da14962ce6d878776eb2caa5c6cc34e0649888
Thanusha Ramesh <thanusharamesh.25@gmail.com>
2020-03-04 12:50:31 +0100

    Added Categories Page

--- online_gift_store/src/App.js
+++ online_gift_store/src/App.js
@@ -1,28 +1,51 @@
 import React from "react";
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'; //react router dom
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; //react router dom
 
 //pages
-import Home from './pages/Home';
-import About from './pages/About';
-import Categories from './pages/Categories';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import Error from './pages/Error';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Categories from "./pages/Categories";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
 //components
-import Header from './components/Header';
+import Header from "./components/Header";
 
-export default function App(){
-  return <Router>
-  <Header />
-  <Switch>
-  <Route exact path="/"> <Home /> </Route>
-    <Route path="/about"> <About /> </Route>
-    <Route path="/categories"> <Categories /> </Route>
-    <Route path="/cart"> <Cart /> </Route>
-    <Route path="/login"> <Login /> </Route>
-    <Route path="*"> <Error /> </Route>
-  </Switch>
-  </Router>
-}  
-
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          {" "}
+          <Home />{" "}
+        </Route>
+        <Route path='/about'>
+          {" "}
+          <About />{" "}
+        </Route>
+        <Route path='/categories'>
+          {" "}
+          <Categories />{" "}
+        </Route>
+        <Route path='/cart'>
+          {" "}
+          <Cart />{" "}
+        </Route>
+        <Route path='/login'>
+          {" "}
+          <Login />{" "}
+        </Route>
+        <Route path='*'>
+          {" "}
+          <Error />{" "}
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
--- online_gift_store/src/pages/Categories.js
+++ online_gift_store/src/pages/Categories.js
@@ -1,5 +1,116 @@
 import React from "react";
+import "./Categories.css";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardTitle,
+  MDBCardText,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+  MDBContainer
+} from "mdbreact";
 
-export default function Categories() {
-  return <h1>hello from categories page</h1>;
-}
+const Categories = () => {
+  return (
+    <MDBContainer size='lg'>
+      <p className='heading'> Categories </p>
+      <MDBRow className='row-margin'>
+        <MDBCol md='4' className='pad'>
+          <MDBCard cascade className='card-ht'>
+            <MDBCardImage
+              cascade
+              className='img-fluid'
+              overlay='white-light'
+              hover
+              src={require("../assets/Birthday.jpg")}
+            />
+
+            <div className='rounded-bottom lighten-3 text-center pt-3 title-css'>
+              <p>Birthday</p>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md='4' className='pad'>
+          <MDBCard cascade className='card-ht'>
+            <MDBCardImage
+              cascade
+              className='img-fluid'
+              overlay='white-light'
+              hover
+              src={require("../assets/Anniversary.jpg")}
+            />
+
+            <div className='rounded-bottom lighten-3 text-center pt-3 title-css'>
+              <p>Anniversary</p>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md='4' className='pad'>
+          <MDBCard cascade className='card-ht'>
+            <MDBCardImage
+              cascade
+              className='img-fluid'
+              overlay='white-light'
+              hover
+              src={require("../assets/House.jpeg")}
+            />
+
+            <div className='rounded-bottom lighten-3 text-center pt-3 title-css'>
+              <p>House Warming Ceremony</p>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md='4' className='pad'>
+          <MDBCard cascade className='card-ht'>
+            <MDBCardImage
+              cascade
+              className='img-fluid'
+              overlay='white-light'
+              hover
+              src={require("../assets/Men.jpg")}
+            />
+
+            <div className='rounded-bottom lighten-3 text-center pt-3 title-css'>
+              <p>Gifts for Men</p>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md='4' className='pad'>
+          <MDBCard cascade className='card-ht'>
+            <MDBCardImage
+              cascade
+              className='img-fluid'
+              overlay='white-light'
+              hover
+              src={require("../assets/Women.jpg")}
+            />
+
+            <div className='rounded-bottom lighten-3 text-center pt-3 title-css'>
+              <p>Gifts for Women</p>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol md='4' className='pad'>
+          <MDBCard cascade className='card-ht'>
+            <MDBCardImage
+              cascade
+              className='img-fluid'
+              overlay='white-light'
+              hover
+              src={require("../assets/Kids.jpg")}
+            />
+
+            <div className='rounded-bottom lighten-3 text-center pt-3 title-css'>
+              <p>Gifts for Kids</p>
+            </div>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default Categories;
